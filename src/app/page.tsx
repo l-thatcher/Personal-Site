e62faa8e99b8bbeb86707c9f09515c7fecebf000
l-thatcher <louis@thatchermob.com>
2024-12-14 13:36:40 +0000

    fixes ssr issues?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 
 import Navbar from "./components/navbar";
@@ -11,21 +11,29 @@ import CVTiles from "./components/CV_tiles";
 import Details from "./components/details_section";
 
 export default function Home() {
+  const [windowWidth, setWindowWidth] = useState(0);
   const parallaxRef = useRef<IParallax>(null!);
+
+  // Update windowWidth on the client side
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
+
+  if (windowWidth === 0) return null; // Prevent rendering before windowWidth is set
 
   return (
     <div>
       <Navbar parallaxRef={parallaxRef} />
       <Parallax
         className="parallax"
-        pages={window.innerWidth < 768 ? 5 : 4}
+        pages={windowWidth < 768 ? 5 : 4}
         ref={parallaxRef}
       >
         <HeroSection />
 
-        <Bio offset={window.innerWidth < 768 ? 0.4 : 1} />
+        <Bio offset={windowWidth < 768 ? 0.4 : 1} />
 
-        <ParallaxLayer speed={0.5} offset={window.innerWidth < 768 ? 4 : 2}>
+        <ParallaxLayer speed={0.5} offset={windowWidth < 768 ? 4 : 2}>
           <div className="flex justify-center absolute">
             <Image
               className=""
@@ -41,7 +49,7 @@ export default function Home() {
         <ParallaxLayer
           speed={0}
           sticky={
-            window.innerWidth < 768
+            windowWidth < 768
               ? { start: 3.7, end: 3.7 }
               : { start: 2.3, end: 2.8 }
           }
@@ -49,7 +57,7 @@ export default function Home() {
           <CVTiles parallaxRef={parallaxRef} />
         </ParallaxLayer>
 
-        <Details offset={window.innerWidth < 768 ? 2 : 2.3} />
+        <Details offset={windowWidth < 768 ? 2 : 2.3} />
       </Parallax>
     </div>
   );
